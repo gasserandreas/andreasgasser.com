49f12c522903bdb2fd2245e060d3ad0039605bc2
Andreas Gasser <andreas.safe@gmail.com>
2019-10-06 18:21:09 +0200

    feat(#1): added styled-components to repo

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,32 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from 'styled-components';
+
+const Styled = {
+  Header: styled.header`
+    background: rebeccapurple;
+    margin-bottom: 1.45rem;
+  `,
+  HeaderText: styled.h1`
+    margin: 0 auto;
+    max-width: 960px;
+    padding: 1.45rem 1.0875rem;
+  `,
+  Link: styled(Link)`
+    color: white;
+    textDecoration: none;
+  `,
+}
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <Styled.Header>
+    <Styled.HeaderText>
+      <Styled.Link to="/">
+        {siteTitle}
+      </Styled.Link>
+    </Styled.HeaderText>
+  </Styled.Header>
 )
 
 Header.propTypes = {
