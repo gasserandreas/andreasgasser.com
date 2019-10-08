95bec7de7b0a350ffbdfc1de7cd6d693c14ef125
Andreas Gasser <andreas.safe@gmail.com>
2019-10-08 13:58:45 +0200

    feat: integrated jest unit test framework
    feat: added test configuration

---
+++ src/components/footer/__tests__/Footer.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+describe('small test', () => {
+  it('test test', () => {
+    expect(true)
+  });
+});
---
+++ src/setupTests.js
@@ -0,0 +1,9 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import jestFetchMock from 'jest-fetch-mock';
+
+// setup enzyme
+configure({ adapter: new Adapter() });
+
+// setup fetch mock
+global.fetch = jestFetchMock;
