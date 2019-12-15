6f743584e673ee737dd58c22769e4007841557f8
Andreas Gasser <andreas.safe@gmail.com>
2019-12-15 22:50:27 +0100

    feat(#27): added first base color set

--- src/components/Theme/themes/dark.js
+++ src/components/Theme/themes/dark.js
@@ -5,6 +5,12 @@ const { colors } = preset;
 export default {
   colors: {
     ...colors,
-    text: '#ccc',
+    // text colors
+    text: '#e9e9e9',
+    'text-inverse': '#9e9e9e',
+    muted: '#808080',
+    // background colors
+    background: '#9a9a9a',
+    'background-inverse': '#323232',
   },
 };
--- src/components/Theme/themes/light.js
+++ src/components/Theme/themes/light.js
@@ -5,6 +5,12 @@ const { colors } = preset;
 export default {
   colors: {
     ...colors,
+    // text colors
     text: '#333',
+    'text-inverse': '#eee',
+    muted: '#c9c9c9',
+    // background colors
+    background: '#656565',
+    'background-inverse': '#fff',
   },
 };
