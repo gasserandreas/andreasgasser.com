74c0c2e80584402cecde3beeaa645f45609c4cfe
Andreas Gasser <andreas.safe@gmail.com>
2019-12-23 17:34:52 +0100

    feat(#42): added new components to temp page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Text, Heading } from 'rebass';
-
 import Page from '../components/Page/Page';
 import SEO from '../components/SEO/SEO';
+
+import Text from '../components/Base/Text';
+import Heading from '../components/Base/Heading';
+import { GitHubIcon, LinkedInIcon } from '../components/Icons/SocialIcon';
 
 const Styled = {
   Heading: styled.div`
     padding: 2rem;
   `,
-  Title: styled(Heading)`
-    text-transform: uppercase;
-    margin: 2rem;
-  `,
-  SubTitle: styled(Text)`
-    margin: 0;
-    font-size: 1.6rem;
-    font-weight: 300;
-    color: #333;
+  SocialIconWrapper: styled.div`
+    a {
+      display: inline-block;
+      margin: 0 0.5rem;
+      font-size: 1.25rem;
+    }
   `,
 };
 
@@ -27,18 +26,22 @@ const IndexPage = () => (
     <div style={{ textAlign: 'center' }}>
       <SEO title="Home" />
       <Styled.Heading>
-        <Styled.Title
-          fontSize={6}
+        <Heading
+          variant="page"
           margin={4}
         >
           Hello you
           {' '}
-        </Styled.Title>
-        <Styled.SubTitle fontSize={4}>and welcome on my new page</Styled.SubTitle>
+        </Heading>
+        <Heading variant="subPage">and welcome on my new page</Heading>
       </Styled.Heading>
       <Text margin={3}>
-        I am currently working on a new version, please return later...
+        I am currently working on a new version, please return later.
       </Text>
+      <Styled.SocialIconWrapper>
+        <GitHubIcon />
+        <LinkedInIcon />
+      </Styled.SocialIconWrapper>
     </div>
   </Page>
 );
