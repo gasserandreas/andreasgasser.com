2b73d673c45675eced3b2f330bfc3d8765e28981
Andreas Gasser <gasserandreas@users.noreply.github.com>
2020-01-12 18:24:30 +0100

    fix(#50): Fixed storybook issue with new TopBar

--- src/components/TopBar/TopBar.stories.jsx
+++ src/components/TopBar/TopBar.stories.jsx
@@ -7,43 +7,51 @@ export default {
   component: TopBar,
 };
 
-export const defaultTopBar = () => (
-  <div style={{
-    position: 'absolute', top: '0px', height: '250px', overflow: 'scroll', borderBottom: '1px solid',
-  }}
-  >
-    <TopBar menuContent={<p>Hello Menu</p>} />
-    <p style={{ paddingTop: TOP_BAR_FULL_HEIGHT }}>
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-      {' '}
-      ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
-      {' '}
-      duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-      {' '}
-      dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-      {' '}
-      tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-      {' '}
-      et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-      {' '}
-      sanctus est Lorem ipsum dolor sit amet.
-      {' '}
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-      {' '}
-      ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
-      {' '}
-      duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-      {' '}
-      dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-      {' '}
-      tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-      {' '}
-      et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-      {' '}
-      sanctus est Lorem ipsum dolor sit amet.
-    </p>
-  </div>
-);
+const ExampleComponent = () => {
+  const ref = React.useRef(null);
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        position: 'absolute', top: '0px', height: '250px', overflow: 'scroll', borderBottom: '1px solid',
+      }}
+    >
+      <TopBar menuContent={<p>Hello Menu</p>} contentRef={ref} />
+      <p style={{ paddingTop: TOP_BAR_FULL_HEIGHT }}>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+        {' '}
+        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
+        {' '}
+        duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+        {' '}
+        dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+        {' '}
+        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+        {' '}
+        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+        {' '}
+        sanctus est Lorem ipsum dolor sit amet.
+        {' '}
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+        {' '}
+        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
+        {' '}
+        duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+        {' '}
+        dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+        {' '}
+        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+        {' '}
+        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+        {' '}
+        sanctus est Lorem ipsum dolor sit amet.
+      </p>
+    </div>
+  );
+};
+
+export const defaultTopBar = () => (<ExampleComponent />);
 
 defaultTopBar.story = {
   name: 'default',
